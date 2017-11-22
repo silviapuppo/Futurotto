a13d81114913c448844b3499de5a54e0e229178e
Gabriele Filaferro <gabriele.filaferro@gmail.com>
2017-11-22 13:15:13 +0100

    wagsd

---
+++ Block04/Exercise06/MyPolynomial.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace Exercise06
+{
+    public class MyPolynomial
+    {
+        private double[] coeffs;
+
+        public double[] Coeffs
+        {
+            get
+            {
+                return coeffs;
+            }
+            set
+            {
+                coeffs = value;
+            }
+        }
+
+        public MyPolynomial(params double[] coeffs)
+        {
+            Coeffs = coeffs;
+        }
+
+        public int GetDegree()
+        {
+            return Coeffs.Length - 1;
+        }
+
+        public override string ToString()
+        {
+            string poly = null;
+
+            for (int i = Coeffs.Length - 1; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+                    if (Coeffs[i] == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        poly += coeffs[i].ToString("+0.####;-0.####") + ' ';
+                    }
+                }
+                else if (i == 1)
+                {
+                    if (Coeffs[i] == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        poly += coeffs[i].ToString("+0.####;-0.####") + "x ";
+                    }
+                }
+                else
+                {
+                    if (Coeffs[i] == 0)
+                    {
+
+                    }
+                    else
+                    {
+                        poly += coeffs[i].ToString("+0.####;-0.####") + "x^" + i + ' ';
+                    }
+                }
+            }
+
+            return poly;
+        }
+
+        public double Evaluate(double x)
+        {
+            double result = 0;
+
+            for (int i = 0; i < Coeffs.Length; i++)
+            {
+                result += Math.Pow(x, i) * Coeffs[i];
+            }
+
+            return result;
+        }
+
+        public MyPolynomial Add(MyPolynomial another)
+        {
+            double[] coeffsfResult = new double[another.GetDegree() > GetDegree() ? another.GetDegree() + 1 : GetDegree() + 1];
+            int i;
+
+            for (i = 0; i < (another.GetDegree() < GetDegree() ? another.GetDegree() + 1 : GetDegree() + 1); i++)
+            {
+                coeffsfResult[i] = Coeffs[i] + another.Coeffs[i];
+            }
+
+            while (i < (another.GetDegree() > GetDegree() ? another.GetDegree() + 1 : GetDegree() + 1))
+            {
+                coeffsfResult[i] = (another.GetDegree() > GetDegree() ? another.Coeffs[i] : Coeffs[i]);
+                i++;
+            }
+
+            return new MyPolynomial(coeffsfResult);
+        }
+
+        public MyPolynomial Multiply(MyPolynomial another)
+        {
+            double[] coeffsResult = new double[GetDegree() + 1 + another.GetDegree() + 1];
+
+            for (int i = 0; i < GetDegree() + 1; i++)
+            {
+                for (int j = 0; j < another.GetDegree() + 1; j++)
+                {
+                    coeffsResult[i + j] += Coeffs[i] * another.Coeffs[j];
+                }
+            }
+
+            return new MyPolynomial(coeffsResult);
+        }
+    }
+}
---
+++ Block04/TestBlock04/TestExercise06.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exercise06;
+
+namespace TestBlock04
+{
+    [TestClass]
+    public class TestExercise06
+    {
+        [TestMethod]
+        public void TestMyPolinomialDegree()
+        {
+            MyPolynomial pol1 = new MyPolynomial(1, 1);
+            MyPolynomial pol2 = new MyPolynomial(2, 2, 2);
+            MyPolynomial pol4 = new MyPolynomial(4, 4, 4, 4, 4);
+
+            Assert.AreEqual(1, pol1.GetDegree());
+            Assert.AreEqual(2, pol2.GetDegree());
+            Assert.AreEqual(4, pol4.GetDegree());
+        }
+
+        [TestMethod]
+        public void TestMyPolinomial()
+        {
+            MyPolynomial pol1 = new MyPolynomial(1, 1);
+            MyPolynomial pol2 = new MyPolynomial(2, 2, 2);
+            MyPolynomial pol4 = new MyPolynomial(4, 4, 4, 0, 0);
+
+            Assert.AreEqual("+2x +2 ", pol1.Add(pol1).ToString());
+            Assert.AreEqual("+2x^2 +3x +3 ", pol1.Add(pol2).ToString());
+            Assert.AreEqual("+4x^2 +5x +5 ", pol1.Add(pol4).ToString());
+        }
+
+        [TestMethod]
+        public void TestMyPolinomialMultiply()
+        {
+            MyPolynomial pol1 = new MyPolynomial(1, 1);
+            MyPolynomial pol2 = new MyPolynomial(2, 2, 2);
+            MyPolynomial pol4 = new MyPolynomial(0, 0, 4, 4, 4);
+
+            Assert.AreEqual("+1x^2 +2x +1 ", pol1.Multiply(pol1).ToString());
+            Assert.AreEqual("+2x^3 +4x^2 +4x +2 ", pol1.Multiply(pol2).ToString());
+            Assert.AreEqual("+4x^5 +8x^4 +8x^3 +4x^2 ", pol1.Multiply(pol4).ToString());
+        }
+
+
+    }
+}
